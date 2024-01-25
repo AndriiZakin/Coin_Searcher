ab6d0c74d15c09bbb9f8b68c4c8667bcbabd9223
AndriiZaikin <1zaikinandrii1@gmail.com>
2024-01-25 21:04:56 +0000

    CI

--- docs/Work_plan.md
+++ docs/Work_plan.md
@@ -1,7 +1,8 @@
 24.01 -> finished the main parts of the simulator 
 - for tommorow I will have to handle some problems in the coin_simulator.py
 - the issue is that the function has unnecesary statement if else
-25.01 ->finished the basics with simulations, may be improved if needed we will se it later. Now try to impove the find_coins and add part like list new added coins or etc.
-- started from the main.py insted of find_coins( well i added init but that's all)
-- write tests
-- I kind of added a CI but needs to be improved
+25.01 -> finished the basics with simulations, may be improved if needed we will se it later. Now try to impove the find_coins and add part like list new added coins or etc. 
+- for tommorow I have to finish the fin_coins and think about any improvement. Consider the thing that the coin just grow by 5200% which is insane, can try to do something with it.
+- check for any issues(add more tests) and add readme file
+- think about incapsulation and etc.
+26.01 ->
--- main.py
+++ main.py
@@ -5,34 +5,39 @@ from find_coins import DataFetcher
 from main_logger import setup_logger
 from main_config import load_configuration
 
-logger = setup_logger()
-coins_list, start_time, amount_usd, target_price, num_coins = load_configuration()
 
-def fetch_symbols():
+class SimulationManager:
+    def __init__(self):
+        self.logger = setup_logger()
+        self.coins_list, self.start_time, self.amount_usd, self.target_price, self.num_coins = load_configuration()
 
-    fetcher = DataFetcher()
-    fetcher.fetch_klines_data("1 Jan, 2017", None)
+    def fetch_symbols(self):
+        fetcher = DataFetcher()
+        fetcher.fetch_klines_data("1 Jan, 2017", None)
 
-def start_simulations():
-    with open(coins_list, 'r') as f:
-        coins = json.load(f)
+    def start_simulations(self):
+        with open(self.coins_list, 'r') as f:
+            coins = json.load(f)
 
-    coins = coins[-num_coins:]
+        coins = coins[-self.num_coins:]
 
-    # Calculate the amount to be used for each coin
-    amount_per_coin = amount_usd / len(coins)
+        # Calculate the amount to be used for each coin
+        amount_per_coin = self.amount_usd / len(coins)
 
-    # Create a new thread for each coin
-    for coin in coins:
-        logger.info(f"Starting simulation for {coin}")
-        threading.Thread(target=simulate_coin_trade, args=(coin, start_time, amount_per_coin, target_price)).start()
+        # Create a new thread for each coin
+        for coin in coins:
+            self.logger.info(f"Starting simulation for {coin}")
+            threading.Thread(target=simulate_coin_trade, args=(coin, self.start_time, amount_per_coin, self.target_price)).start()
+
 def main():
+    manager = SimulationManager()
+
     # Fetch symbols
-    fetch_symbols()
+    manager.fetch_symbols()
 
     # Start simulations
-    logger.info("Starting simulations")
-    start_simulations()
+    manager.logger.info("Starting simulations")
+    manager.start_simulations()
 
 if __name__ == "__main__":
     main()
