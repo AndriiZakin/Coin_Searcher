7dbf32f105af87c170a3e1691000c1c18598deee
AndriiZaikin <1zaikinandrii1@gmail.com>
2024-01-25 21:20:02 +0000

    test_CI

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,7 +2,6 @@ import unittest
 import os
 import sys
 
-# Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == "__main__":
