80944f32e8b7a0ce621c78e96dd6f4f6c1016cc8
AndriiZaikin <1zaikinandrii1@gmail.com>
2024-01-25 21:12:54 +0000

    runner

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,4 +1,9 @@
 import unittest
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
